2d5e3e6d4c62661f3819c52b9bf8ac46d1332d7f
Yaw Agyepong <yaw.agyepong@gmail.com>
2025-05-30 20:57:22 -0400

    attempting to resolve issue where home button fails to navigate when offline

--- src/app/sw.ts
+++ src/app/sw.ts
@@ -20,6 +20,16 @@ const serwist = new Serwist({
   clientsClaim: true,
   navigationPreload: true,
   runtimeCaching: defaultCache,
+  fallbacks: {
+    entries: [
+      {
+        url: "/agua-vida-poc",
+        matcher({ request }) {
+          return request.destination === "document";
+        },
+      },
+    ],
+  },
 });
 
 serwist.addEventListeners();
