571a5827b589d2ca7903b1fe775955db196655cd
Yaw Agyepong <yaw.agyepong@gmail.com>
2025-05-30 20:17:08 -0400

    Revert "remove base path in next config and let github actions handle it. additionally, update fixup script to append basepath to manifest file path"
    This reverts commit 73de413dc0ae5f16dfc144fa62ba2cea9f7c096b.

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,15 @@
 import withSerwistInit from "@serwist/next";
 import type { NextConfig } from "next";
+// const path = require('path');
 
 const withSerwist = withSerwistInit({
   swSrc: 'src/app/sw.ts',
-  swDest: 'public/sw.js',
-  maximumFileSizeToCacheInBytes: 5 * 1024 ** 2 //TODO: massively reduce image sizes so this isn't necessary
+  swDest: 'public/sw.js'
 })
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/agua-vida-poc',
 };
 
 export default withSerwist(nextConfig);
