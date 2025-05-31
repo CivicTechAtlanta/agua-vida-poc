dd3c40048443cdd0eace93fbf1f895437ab265ed
Yaw Agyepong <yaw.agyepong@gmail.com>
2025-05-30 20:40:13 -0400

    increase max file size to cache (hopefully) temporarily due to image assets

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import withSerwistInit from "@serwist/next";
 import type { NextConfig } from "next";
-// const path = require('path');
 
 const withSerwist = withSerwistInit({
   swSrc: 'src/app/sw.ts',
-  swDest: 'public/sw.js'
+  swDest: 'public/sw.js',
+  maximumFileSizeToCacheInBytes: 5 * 1024 ** 2 //TODO: massively reduce image sizes so this isn't necessary
 })
 
 const nextConfig: NextConfig = {
