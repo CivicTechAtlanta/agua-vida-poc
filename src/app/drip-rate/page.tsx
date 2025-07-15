b61147f722278f2a9b7f0a8d44a074f268ce762d
Yaw Agyepong <yaw.agyepong@gmail.com>
2025-07-14 23:01:55 -0400

    Created drip rate calculator

---
+++ src/app/drip-rate/page.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+import LanguageSelector from "../components/LanguageSelector/LanguageSelector";
+
+import './DripRate.scss';
+import Home from "../components/Home/Home";
+
+export default function DripRateFormula() {
+    const { t } = useTranslation();
+
+    const [motherSolutionVolume, setMotherSolutionVolume] = useState<number>(0)
+    const [dripRate, setDripRate] = useState<number>(0)
+    const [refillTime, setRefillTime] = useState<number>(0)
+
+    useEffect(() => {
+        const calculatedDripRate = refillTime > 0 ? (motherSolutionVolume * 1000) / (refillTime * 1440) : 0
+        setDripRate(calculatedDripRate)
+    }, [motherSolutionVolume, refillTime])
+
+    return (
+        <div className="center">
+            <Home></Home>
+            <h1>{t('Drip Rate Formula')}</h1>
+            <div className="input-wrapper">
+
+                <div className="input-group">
+                    <label>{`${t('Mother Solution Volume')} (${t('L')})`}</label>
+                    <input type="number" value={motherSolutionVolume} onChange={(event) => { setMotherSolutionVolume(Number(event.target.value)) }} />
+                </div>
+
+                <div className="input-group">
+                    <label>{`${t('Refill time')} (${t('days')})`}</label>
+                    <input type="number" value={refillTime} onChange={(event) => { setRefillTime(Number(event.target.value)) }} />
+                </div>
+
+                <h2>{`${t('Drip Rate is')}: ${dripRate.toFixed(2)} ${t('milliliters')}/${t('minute')}`}</h2>
+
+                <button className="button" onClick={() => {
+                    setMotherSolutionVolume(0)
+                    setRefillTime(0)
+                }}>{t('Clear')}</button>
+            </div>
+
+            <LanguageSelector></LanguageSelector>
+        </div>
+    );
+}
