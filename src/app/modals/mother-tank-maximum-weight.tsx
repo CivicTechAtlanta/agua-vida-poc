5bfadf9fb61bce8996cd88b8d3f62aee06c64c71
Steve Chan <stephen.albaran.chan@gmail.com>
2025-08-17 20:16:23 -0400

    finish adding images

--- src/app/modals/mother-tank-maximum-weight.tsx
+++ src/app/modals/mother-tank-maximum-weight.tsx
@@ -1,9 +1,9 @@
 const modalData = {
     pages : [
         {
-            headerText: 'mother solution max weight header 1',
-            modalText: 'mother solution max weight text 1',
-            imageKey: null
+            headerText: "mother solution max weight header 1",
+            modalText: "mother solution max weight text 1",
+            imageKey: "MOTHER_SOLUTION_MAX_WEIGHT_1"
         }
     ]
 };
